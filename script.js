60e215b66d6f61aea3ef652a0d2b277cddc14281
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-27 13:23:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+const btnClick = document.getElementById("btn");
+
+btnClick.addEventListener("click", function() {
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
+
+    if (name === "" || age === "") {
+        alert("Please enter valid details");
+        return; // Important: stop execution
+    }
+
+    promiseDec(age).then((data) => {
+        alert(data);
+    }).catch((err) => {
+        alert(err);
+    });
+});
+
+function promiseDec(age) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (age >= 18) {
+                resolve("Welcome, You can vote");
+            } else {
+                reject("Oh sorry. You aren't old enough.");
+            }
+        }, 4000);
+    });
+}
